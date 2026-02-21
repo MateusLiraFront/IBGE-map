d39ccb263fe4f03d4986f8b9443508b7af1262a0
MateusLiraFront <mlgtifront@gmail.com>
2026-02-21 11:47:03 -0300

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Mapa - Regiões do Brasil</h1>
+      <img src='./public/br.svg'></img>
+      <svg src="./"></svg>
+    </>
+  )
+}
+
+export default App
